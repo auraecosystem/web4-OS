ceb4c9d9041d01e0f946f465bcf2e61a6328d3bc
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-25 13:22:10 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import HomePage from "@pages/Home";
+import Navbar from "@components/Navbar";
+import { useWeb4 } from "@web4/useWeb4";
+import { LmlmClient } from "@lmlm/client";
+import { WalletProvider } from "@qubuhub/wallet";
